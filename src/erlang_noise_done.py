Fixed memory leakage issues...

src/erlang_noise_done.py
@@ -23,7 +23,7 @@ import numpy as np
 #cv2.imwrite('output_image.png', img_with_noise)
 
 
-def erlang_noise(img_path):
+def erlang_noise(img):
     # import numpy as np
     # import scipy
     # from scipy.signal import cwt
@@ -145,7 +145,7 @@ def erlang_noise(img_path):
 
     scales = np.arange(1, 10)
     # img = cv2.imread(img_path,0)
-    img = img_path.copy()
+    # img = img_path.copy()
     # print(img.shape)
     # cwtmatr, freqs = sig.cwt2d(image, erlang2d_wavelet, scales, theta=5, scale=2)
     cwtmatr, freqs = cwt_2d(img,scales,'erlang',theta=5, scale=2)

src/exponential_noise_done.py
@@ -20,7 +20,7 @@ noisy_pixels_inv = cv2.bitwise_and(inv_image, inv_image, mask=mask)
 noisy_pixels = 1/noisy_pixels_inv
 '''
 
-def exponential_noise(img_path):
+def exponential_noise(img):
     # import numpy as np
     # import scipy
     # from scipy.signal import cwt
@@ -93,7 +93,7 @@ def exponential_noise(img_path):
 
     scales = np.arange(1, 10)
     # img = cv2.imread(img_path,0)
-    img = img_path.copy()
+    # img = img_path.copy()
     # print(img.shape)
     # cwtmatr, freqs = sig.cwt2d(image, erlang2d_wavelet, scales, theta=5, scale=2)
     cwtmatr, freqs = cwt_2d(img,scales,'exponential',tau=10)

src/gaussian_noise_done.py
@@ -219,7 +219,7 @@ import numpy as np
 #cv2.imwrite('output_image.png', img_with_noise)
 
 
-def gaussian_noise(img_path):
+def gaussian_noise(img):
     
     from skimage.io import imread
     import matplotlib.pyplot as plt
@@ -300,7 +300,7 @@ def gaussian_noise(img_path):
 
     scales = np.arange(1, 10)
     # img = cv2.imread(img_path,0)
-    img = img_path.copy()
+    # img = img_path.copy()
     cwtmatr, freqs = cwt_2d(img,scales,'gaussian')
     cwt_result = np.abs(cwtmatr)
 

src/poisson_noise_done.py
@@ -31,7 +31,7 @@ noisy_pixels = cwt_result > threshold
 # values indicating the presence of noisy pixels in the image
 '''
 
-def poisson_noise(img_path):
+def poisson_noise(img):
     from skimage.io import imread
     import matplotlib.pyplot as plt
     import numpy as np
@@ -97,7 +97,7 @@ def poisson_noise(img_path):
 
     scales = np.arange(1, 10)
     # img = cv2.imread(img_path,0)
-    img = img_path.copy()
+    # img = img_path.copy()
     # print(img.shape)
     # cwtmatr, freqs = sig.cwt2d(image, erlang2d_wavelet, scales, theta=5, scale=2)
     cwtmatr, freqs = cwt_2d(img,scales,'poisson',sigma=1)

src/speckle_noise_done.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-def speckle_noise(img_path, threshold=1.5):
+def speckle_noise(image, threshold=1.5):
     # image = cv2.imread(img_path,0)
-    image = img_path.copy()
+    # image = img_path.copy()
     # Calculate the mean and standard deviation of the image
     mean, std = np.mean(image), np.std(image)
     